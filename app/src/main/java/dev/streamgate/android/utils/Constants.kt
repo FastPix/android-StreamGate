0d8a1a9580f5bc36e2b0577e9b31f28b6113feef
Rohit Yadav <rohit.y@fastpix.com>
2026-06-18 15:27:46 +0530

    migrated fastpix.io to .com tld

--- app/src/main/java/dev/streamgate/android/utils/Constants.kt
+++ app/src/main/java/dev/streamgate/android/utils/Constants.kt
@@ -1,8 +1,8 @@
 package dev.streamgate.android.utils
 
 
-private const val HOST_URL = "fastpix.io"
+private const val HOST_URL = "fastpix.com"
 private const val API_VERSION = "v1"
 const val API_BASE_URL = "https://api.${HOST_URL}/${API_VERSION}/"
-const val VIDEO_SHARE_URL = "https://play.fastpix.io/?playbackId=%s&muted=true&hide-controls=true&accent-color=ff6100&primary-color=ffffff"
+const val VIDEO_SHARE_URL = "https://play.fastpix.com/?playbackId=%s&muted=true&hide-controls=true&accent-color=ff6100&primary-color=ffffff"
 const val APP_TERMS_URL = "https://streamgate.dev/terms"
